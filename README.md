ac684eec2dab505183f8899ddb79f6bd8c61d7c5
fjorty <naz.lavryk@gmail.com>
2019-03-11 22:28:15 +0200

    clear

--- README.md
+++
@@ -1,3 +0,0 @@
-# ES6_Task_2
-
-A simple page for finding movies and TV shows.
--- src/js/config.js
+++
@@ -1,7 +0,0 @@
-const API_KEY = `3d34828e5c71c80272fde06fbd627191`;
-
-export default {
-    searchMovieUrl: `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=`,
-    imageSrc: `http://image.tmdb.org/t/p/w185`,
-    noImageSrc: `https://i.postimg.cc/Zq8Bq3Zt/default-no-image.jpg`
-}
